8e4e8050967a8d221525d3250960a0e80f6ad69b
NGEddie <41394346+NGEddie@users.noreply.github.com>
2018-07-27 20:27:03 +0100

    End of Lecture 51 - Obstcles

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -59,4 +59,17 @@ public class Rocket : MonoBehaviour
         rigidBody.freezeRotation = false;
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                print("Bump");
+                break;
+            default:
+                print("You're Dead");
+                break;
+        }
+    }
+
 }
